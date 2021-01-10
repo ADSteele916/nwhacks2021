d541b9d123a680450dda534e769e3896ded143a2
Alex Steele <45648397+ADSteele916@users.noreply.github.com>
2021-01-09 21:56:52 -0800

    Fix courses

--- grades/views.py
+++ grades/views.py
@@ -5,6 +5,7 @@ from django.template import loader
 from .models import Course, Bin, Assessment
 from .forms import Courseform
 
+
 def home(request):
     return render(request, "grades/home.html")
 
@@ -16,30 +17,37 @@ def faq(request):
 def about(request):
     return render(request, "grades/about.html", {"title": "About"})
 
+
 @login_required
 def courses(request):
-    courses_list = Course.objects.get(user=request.user)
+    courses_list = Course.objects.filter(user__user=request.user)
     template = loader.get_template("grades/courses.html")
     context = {
         'courses_list': courses_list,
     }
     return HttpResponse(template.render(context, request))
+
+
 @login_required
 def course(request, course_id):
-    bins_list = Bin.objects.filter(course__pk=course_id)
+    bins_list = Bin.objects.get(course__pk=course_id)
     template = loader.get_template("grades/course.html")
     context = {
         'bins_list': bins_list,
     }
     return HttpResponse(template.render(context, request))
+
+
 @login_required
 def assessment(request, course_id, bin_id):
-    assessments_list = Assessment.objects.filter(bin__pk=bin_id)
+    assessments_list = Assessment.objects.get(bin__pk=bin_id)
     template = loader.get_template("grades/assessment.html")
     context = {
         'assessments_list': assessments_list,
     }
     return HttpResponse(template.render(context, request))
+
+
 @login_required
 def newCourse(request):
     if request.method == 'POST':
