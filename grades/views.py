d0b6cd398a2eef19a8c7af741b5124e28e44222b
Evan Liu <liuyievan@gmail.com>
2021-01-09 21:37:31 -0800

    Dynamic user

--- grades/views.py
+++ grades/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
@@ -15,14 +16,15 @@ def faq(request):
 def about(request):
     return render(request, "grades/about.html", {"title": "About"})
 
+@login_required
 def courses(request, user_id):
-    courses_list = Course.objects.filter(user__pk=user_id)
+    courses_list = Course.objects.filter(user=request.user)
     template = loader.get_template("grades/courses.html")
     context = {
         'courses_list': courses_list,
     }
     return HttpResponse(template.render(context, request))
-
+@login_required
 def course(request, user_id, course_id):
     bins_list = Bin.objects.filter(course__pk=course_id)
     template = loader.get_template("grades/course.html")
@@ -30,7 +32,7 @@ def course(request, user_id, course_id):
         'bins_list': bins_list,
     }
     return HttpResponse(template.render(context, request))
-
+@login_required
 def assessment(request, user_id, course_id, bin_id):
     assessments_list = Assessment.objects.filter(bin__pk=bin_id)
     template = loader.get_template("grades/assessment.html")
@@ -38,7 +40,7 @@ def assessment(request, user_id, course_id, bin_id):
         'assessments_list': assessments_list,
     }
     return HttpResponse(template.render(context, request))
-
+@login_required
 def newCourse(request, user_id):
     if request.method == 'POST':
         form = Courseform(request.POST)
