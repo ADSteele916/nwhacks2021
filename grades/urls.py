f003e27083edbe5331ff84df55d0cf4af48bab9c
Evan Liu <liuyievan@gmail.com>
2021-01-09 20:13:10 -0800

    Bins support

--- grades/urls.py
+++ grades/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.home, name="grades-home"),
     path("faq/", views.faq, name="grades-faq"),
     path("about/", views.about, name="grades-about"),
-    path("courses/", views.courses, name="grades-courses")
+    path("courses/", views.courses, name="grades-courses"),
+    path("course/<int:course_id>/", views.course, name='grades-course')
 ]
