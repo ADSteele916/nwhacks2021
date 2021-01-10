4aecbf2cca607dbdcb68894b8f0595c7325250e6
Alex Steele <45648397+ADSteele916@users.noreply.github.com>
2021-01-09 20:54:56 -0800

    Add grade calculating functionality to courses

--- grades/models.py
+++ grades/models.py
@@ -9,6 +9,15 @@ class Course(models.Model):
     def __str__(self):
         return self.name
 
+    def get_grade(self) -> float:
+        weights = 0.0
+        rsf = 0.0
+        for b in self.bin_set.all():
+            weights += b.weight
+            rsf += b.weight * b.get_grade()
+        return rsf / weights * 100.0
+
+
 class Bin(models.Model):
     name = models.CharField(max_length=20)
     weight = models.PositiveSmallIntegerField()
@@ -18,11 +27,25 @@ class Bin(models.Model):
     def __str__(self):
         return self.name
 
+    def get_grade(self) -> float:
+        weighted_assignments = [(a.weight, a.get_grade() * a.weight) if a.mark is not None else (0.0, 0.0) for a in self.assessment_set.all()]
+        weighted_marks = [wa[1] for wa in weighted_assignments]
+        for i in range(self.drop_n_lowest):
+            idx = weighted_marks.index(min(weighted_marks))
+            weighted_marks.pop(idx)
+            weighted_assignments.pop(idx)
+        return sum(weighted_assignments)
+
+
 class Assessment(models.Model):
     name = models.CharField(max_length=20)
     weight = models.PositiveSmallIntegerField()
+    total = models.PositiveSmallIntegerField()
     mark = models.PositiveSmallIntegerField(validators=[MaxValueValidator(100), MinValueValidator(0)], null=True)
     bin = models.ForeignKey(Bin, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+
+    def get_grade(self) -> float:
+        return self.mark / self.total
