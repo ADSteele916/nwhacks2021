b5fe0b8780a62aa1b3d396299c931df3f41f1a84
Alex Steele <45648397+ADSteele916@users.noreply.github.com>
2021-01-09 16:40:00 -0800

    Add grades models

--- grades/models.py
+++ grades/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=10)
+    credits = models.PositiveSmallIntegerField()
+
+
+class Bin(models.Model):
+    name = models.CharField(max_length=20)
+    weight = models.PositiveSmallIntegerField()
+    drop_n_lowest = models.PositiveSmallIntegerField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+
+class Assessment(models.Model):
+    name = models.CharField(max_length=20)
+    weight = models.PositiveSmallIntegerField()
+    bin = models.ForeignKey(Bin, on_delete=models.CASCADE)
