45be227b332199c8e8dbad0b40e3546f03588edf
Evan Liu <liuyievan@gmail.com>
2021-01-09 21:25:41 -0800

    Users now have courses

--- grades/urls.py
+++ grades/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path("", views.home, name="grades-home"),
     path("faq/", views.faq, name="grades-faq"),
     path("about/", views.about, name="grades-about"),
-    path("courses/", views.courses, name="grades-courses"),
-    path("course/<int:course_id>/", views.course, name='grades-course'),
-    path("course/<int:course_id>/<int:bin_id>", views.assessment, name='grades-assessment'),
-    path("courses/new", views.newCourse, name="grades-new-course")
+    path("<int:user_id>/courses/", views.courses, name="grades-courses"),
+    path("<int:user_id>/course/<int:course_id>/", views.course, name='grades-course'),
+    path("<int:user_id>/course/<int:course_id>/<int:bin_id>", views.assessment, name='grades-assessment'),
+    path("<int:user_id>/courses/new", views.newCourse, name="grades-new-course")
 ]
--- grades/views.py
+++ grades/views.py
@@ -15,15 +15,15 @@ def faq(request):
 def about(request):
     return render(request, "grades/about.html", {"title": "About"})
 
-def courses(request):
-    courses_list = Course.objects.order_by('-name')
+def courses(request, user_id):
+    courses_list = Course.objects.filter(user__pk=user_id)
     template = loader.get_template("grades/courses.html")
     context = {
         'courses_list': courses_list,
     }
     return HttpResponse(template.render(context, request))
 
-def course(request, course_id):
+def course(request, user_id, course_id):
     bins_list = Bin.objects.filter(course__pk=course_id)
     template = loader.get_template("grades/course.html")
     context = {
@@ -31,7 +31,7 @@ def course(request, course_id):
     }
     return HttpResponse(template.render(context, request))
 
-def assessment(request, course_id, bin_id):
+def assessment(request, user_id, course_id, bin_id):
     assessments_list = Assessment.objects.filter(bin__pk=bin_id)
     template = loader.get_template("grades/assessment.html")
     context = {
@@ -39,7 +39,7 @@ def assessment(request, course_id, bin_id):
     }
     return HttpResponse(template.render(context, request))
 
-def newCourse(request):
+def newCourse(request, user_id):
     if request.method == 'POST':
         form = Courseform(request.POST)
 
